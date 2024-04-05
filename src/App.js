66c795393dfcb21e14ff5526b0c0e16f1a2c8f8c
Muhammed Hanifi Dikmen <dikmen@live.com>
2024-04-05 23:40:27 +0300

    Compound component pattern added

--- src/App.js
+++ src/App.js
@@ -5,14 +5,32 @@ export default function App() {
   return (
     <div>
       <h1>Compound Component Pattern</h1>
-      <Counter
+      {/* <Counter
         iconIncrease="+"
         iconDecrease="-"
         label="My NOT so flexible counter"
         hideLabel={false}
         hideIncrease={false}
         hideDecrease={false}
-      />
+      /> */}
+
+      <Counter>
+        <Counter.Label>My super flexible counter</Counter.Label>
+        <Counter.Decrease icon="-" />
+        <Counter.Count />
+        <Counter.Increase icon="+" />
+      </Counter>
+      <div>
+        <Counter>
+          <div>
+            <Counter.Decrease icon="<" />
+          </div>
+          <Counter.Count />
+          <div>
+            <Counter.Increase icon=">" />
+          </div>
+        </Counter>
+      </div>
     </div>
   );
 }
